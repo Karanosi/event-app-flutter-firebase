207f1bb89315474d0cbecfa269ad316ff91659d3
BENALI OULAHCEN Ikram <105321428+Karanosi@users.noreply.github.com>
2023-01-08 00:32:09 +0100

    update 1
    added bottom navbar and some documents to structure it

---
+++ lib/signin.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Créer votre compte')),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
